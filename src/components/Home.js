5cc0b5f38627f2631e547b01cac32ef3d453d5d9
rbaliwal00 <rajan18baliwal@gmail.com>
2022-09-18 14:40:20 +0530

    ui 1.0 upgrade

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,7 +6,7 @@ const Home = () => {
   return (
     <div className='text-center w-3/5 m-auto p-10 rounded-xl mt-36' style={{background:'#c9ffe5'}}>
       <p className='text-6xl mb-20'>Optimize your Bill</p>
-      <Link to={"/selectappliances"}><button className='px-5 py-2   mt-5 rounded-lg' style={{background:'teal'}}><p className='' style={{color:'white'}}>Click here to get Started!!!</p></button></Link>
+      <Link to={"/show"}><button className='px-5 py-2   mt-5 rounded-lg' style={{background:'teal'}}><p className='' style={{color:'white'}}>Click here to get Started!!!</p></button></Link>
     </div>
   )
 }
